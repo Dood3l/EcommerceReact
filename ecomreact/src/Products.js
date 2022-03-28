39a9b567a82736040a5e3435813e507fb6649cca
Dood3l <bk020220@gmail.com>
2022-03-27 20:57:40 -0400

    added filter function

--- ecomreact/src/Products.js
+++ ecomreact/src/Products.js
@@ -4,17 +4,6 @@ import Productlist from './components/getProducts'
 
 function Product() {
     return(
-        // CARD 1
-        <div>
-            <h6>Filter: </h6>
-        <div id="fButtons">
-            <button id="button" class="btn btn-light" onclick="filterProduct">Anime</button>
-            <button id="button" class="btn btn-light" onclick="filterProduct">Synth</button>
-            <button id="button" class="btn btn-light" onclick="filterProduct">Contrasted</button>
-        </div>
-        <div>
         <Productlist />
-        </div>    
-      </div>
     )}
 export default Product
