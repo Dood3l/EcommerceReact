efb8eb3e1dc60b2f362dd05326964ab29fcf10b5
Dood3l <bk020220@gmail.com>
2022-03-29 00:29:00 -0400

    browser

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Home from './Home';
 import Product from './Products';
 import Contact from './Contact';
 import Cart from './Cart';
-import { HashRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 
 // Used HashRouter instead of BrowserRouter in order to refresh page
 function App() {
   return (
     <>
-      <HashRouter>
+      <Router>
       <Header />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -21,7 +21,7 @@ function App() {
           <Route path="/cart" element={<Cart />} />
         </Routes>
       <Footer />
-      </HashRouter>
+      </Router>
     </>
   );
 }
