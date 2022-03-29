ddf7c466abceceffaa95cc4a6f0045b3093e8498
Dood3l <bk020220@gmail.com>
2022-03-29 18:51:18 -0400

    server disc

--- server.js
+++ server.js
@@ -6,30 +6,20 @@ const mysql = require('mysql');
 const app = express();
 // Above this line are the dependencies
 
-let db;
-console.log(process.env)
-db = mysql.createConnection({
+
+
+var db_config = {
     host: 'us-cdbr-east-05.cleardb.net',
     user: 'b09453fff7c96a',
     password: '43adde49',
     database: 'heroku_e1c0b0c780ef5e7'
-});
+};
 // Above this line is the connection between React and Heroku mySQL cloud.
-
-db.connect(err => {
-    if(err) {
-        console.log('from db', err)
-        return err;
-    } else {
-        console.log("db connection successful!")
-    }
-});
-// Above this line is a function that checked if the connection between React and mySQL is successful.
 
 app.use(cors());
 app.use(express.json());
 app.get('/api/products', (req, res) => {
-    db.query('SELECT * FROM products', (err, result) => {
+    connection.query('SELECT * FROM products', (err, result) => {
         if (err) {
             console.log(err)
         }else {
@@ -49,3 +39,24 @@ app.listen(process.env.PORT || 5000, function(){
     console.log(`Express is working on port 5000`)
 });
 // Above this line is a function that pushes the server to either a port set dynamically by Heroku, or it defaults to port 5000.
+
+function handleDisconnect() {
+    connection = mysql.createConnection(db_config); 
+  
+    connection.connect(function(err) {              
+      if(err) {                                    
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); 
+      }                                     
+    });                                     
+                                            
+    connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+        handleDisconnect();                         
+      } else {                                      
+        throw err;                                  
+      }
+    });
+  }
+handleDisconnect();
