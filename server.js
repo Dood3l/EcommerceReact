11c6e17ced6c90bb4a106c628908adefa1f707bb
Dood3l <bk020220@gmail.com>
2022-03-29 17:37:28 -0400

    changed back

--- server.js
+++ server.js
@@ -44,13 +44,13 @@ app.get('/api/products', (req, res) => {
         }
     })
 });
-
 app.use(express.static(path.join(__dirname, './build')));
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, './build', 'index.html'))
 })
 
 app.listen(process.env.PORT || 5000, () => {
+    const port = server.address().port
     console.log(`Express is working on port ${port}`)
 });
 
